87fb5c3322ae1dafc66b204b1d771e2bf8d8dc7d
Thomas Karmann <thomas@krmnn.de>
2013-03-25 17:12:04 -0400

    remove unneeded imports

--- wall/tagesschau/__init__.py
+++ wall/tagesschau/__init__.py
@@ -7,8 +7,6 @@ from __future__ import (division, absolute_import, print_function,
 from wall import Brick as _Brick, randstr
 
 import feedparser
-import json, urllib2
-import pprint
 
 class Brick(_Brick):
     id        = 'tagesschau'
