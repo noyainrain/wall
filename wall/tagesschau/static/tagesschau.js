c84a9091ec208eda51fd164860392a4989227594
Thomas Karmann <thomas@krmnn.de>
2013-03-25 18:00:29 -0400

    fixed

--- wall/tagesschau/static/tagesschau.js
+++ wall/tagesschau/static/tagesschau.js
@@ -7,7 +7,6 @@ wall.tagesschau = {};
 
 ns.Brick = function(ui) {
     this.ui     = ui;
-    this.window = null;
 };
 
 ns.Brick.prototype = {
@@ -15,14 +14,11 @@ ns.Brick.prototype = {
     postTitle: "Tagesschau",
     
     initPost: function(elem, post) {
-		foo = "<center><video id=\"video1\" autoplay>" + "<source src=\"" + post.url + "\" type=\'video/mp4; codecs=\"avc1.42E01E, mp4a.40.2\"\' />" + "<video></center>";
+		foo = "<center><video id=\"video1\" autoplay>" + "<source src=\"" + post.url + "\" type=\'video/mp4; codecs=\"avc1.42E01E, mp4a.40.2\"\' />" + "</video></center>";
 		elem.html(foo);
     },
     
     cleanupPost: function() {
-		if (this.window) {
-        	this.window.close();
-		}
     },
     
     clientInitPost: function(elem, post) {
