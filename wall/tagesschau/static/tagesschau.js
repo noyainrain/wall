517583aafcccf6c5d9661e8db1fa6a16d4e4f4aa
Philip Taffner <philip.taffner@bluegfx.de>
2013-04-03 21:57:04 +0200

    add missing comma

--- wall/tagesschau/static/tagesschau.js
+++ wall/tagesschau/static/tagesschau.js
@@ -13,7 +13,7 @@ $.extend(ns.Brick.prototype, wall.Brick.prototype, {
     id:        "tagesschau",
     postType:  "TagesschauPost",
     postTitle: "Tagesschau",
-    postSingle: true
+    postSingle: true,
     
     initPost: function(elem, post) {
 		elem.html("<video style=\"display: block; margin: 0 auto;\" id=\"video1\" autoplay>" + "<source src=\"" + post.url + "\" type=\'video/mp4; codecs=\"avc1.42E01E, mp4a.40.2\"\' />" + "<video>");
