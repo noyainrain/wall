98bc51669922e6ee8dc59cf42eafc48ccaf9b987
Thomas Karmann <thomas@krmnn.de>
2013-04-11 09:44:35 -0400

    make tagesschau video fullscreen

--- wall/bricks/tagesschau/static/tagesschau.js
+++ wall/bricks/tagesschau/static/tagesschau.js
@@ -16,7 +16,13 @@ $.extend(ns.Brick.prototype, wall.Brick.prototype, {
     postSingle: true,
     
     initPost: function(elem, post) {
-		elem.html("<video style=\"display: block; margin: 0 auto;\" id=\"video1\" autoplay>" + "<source src=\"" + post.url + "\" type=\'video/mp4; codecs=\"avc1.42E01E, mp4a.40.2\"\' />" + "<video>");
+		elem.html("<video style=\"display: block; margin: 0 auto;\" id=\"tagesschau-video-tag\" autoplay>" + "<source src=\"" + post.url + "\" type=\'video/mp4; codecs=\"avc1.42E01E, mp4a.40.2\"\' />" + "<video>");
+        // vertical align
+        $('#tagesschau-video-tag').on('loadedmetadata', function() {
+            $(this).css('margin-top', -($(this).height() / 2));
+        });
+
+
     },
     
     clientInitPost: function(elem, post) {
