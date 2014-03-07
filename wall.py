d829b9ac43df04ba8bafbd2617fcabf2c1ab747f
Sven <sven.jms@gmail.com>
2014-03-07 13:38:23 +0100

    added release number / information

--- wall.py
+++ wall.py
@@ -7,11 +7,13 @@ from __future__ import (division, absolute_import, print_function,
     unicode_literals)
 
 import sys
+import wall
 from wall import WallApp
 
 if __name__ == '__main__':
     # TODO: use option instead
     config_path = sys.argv[1] if len(sys.argv) >= 2 else None
+    print('Wall #{}'.format(wall.release))
     print('display: http://localhost:8080/display')
     print('client:  http://localhost:8080/')
     WallApp(config_path=config_path).run()
