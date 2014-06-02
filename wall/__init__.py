c3edc1b19a3a11b8d0c7d01f8c2f6d5bc3c8acf8
Sven <sven.jms@gmail.com>
2014-06-02 17:41:21 +0200

    bumped release

--- wall/__init__.py
+++ wall/__init__.py
@@ -22,7 +22,7 @@ from tornado.websocket import WebSocketHandler
 from redis import StrictRedis
 from wall.util import EventTarget, Event, RedisContainer, truncate
 
-release = 13
+release = 14
 
 res_path = os.path.join(os.path.dirname(__file__), 'res')
 static_path = os.path.join(res_path, 'static')
