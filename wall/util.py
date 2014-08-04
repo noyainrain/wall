e51187f09f7a9c44eb5385d84b8ac3f796310e94
Sven <sven.jms@gmail.com>
2014-08-04 16:44:27 +0200

    implemented ObjectRedis and integrated it into RedisContainer

--- wall/util.py
+++ wall/util.py
@@ -8,6 +8,7 @@ import sys
 import json
 from urllib import urlencode
 from collections import Mapping
+from weakref import WeakValueDictionary
 from tornado.httpclient import AsyncHTTPClient
 from redis import StrictRedis
 
@@ -84,28 +85,79 @@ class Event(object):
         self.target = None
         self.args = dict(args.items() + kwargs.items())
 
+class ObjectRedis(object):
+    """
+    Extended Redis client, which additionally provides object-oriented
+    operations and object caching.
+
+    Objects are represented as hashes in the Redis database. The translation
+    from a hash to an object is carried out by a given `decode` function.
+
+    When `caching` is enabled, objects loaded from the Redis database are cached
+    and subsequently retrieved from the cache. An object stays in the cache as
+    long as there is a reference to it and it is automatically removed when the
+    Python interpreter destroys it. Thus, it is guaranteed that getting the same
+    key multiple times will yield the identical object.
+
+    Attributes:
+
+     * `r`: Underlying Redis client. Read-Only.
+     * `decode`: function, which decodes an object from a Redis hash. It is
+       called with the hash (a `dict`) as single argument. Read-Only.
+     * `caching`: switch to enable / disable object caching.
+    """
+    # TODO: add oset and omset
+
+    def __init__(self, r, decode, caching=True):
+        self.r = r
+        self.decode = decode
+        self.caching = caching
+        self._cache = WeakValueDictionary()
+
+    def oget(self, key):
+        """
+        Get the object for `key`.
+        """
+        object = self._cache.get(key) if self.caching else None
+        if not object:
+            hash = self.hgetall(key)
+            if hash:
+                object = self.decode(hash)
+                if self.caching:
+                    self._cache[key] = object
+        return object
+
+    def omget(self, keys):
+        """
+        Get the objects for all specified `keys`.
+        """
+        # TODO: make atomic
+        return [self.oget(k) for k in keys]
+
+    def __getattr__(self, name):
+        return getattr(self.r, name)
+
 class RedisContainer(Mapping):
-    def __init__(self, db, set_key, cls):
-        self.db = db
+    def __init__(self, r, set_key):
+        self.r = r
         self.set_key = set_key
-        self.cls = cls
 
     def keys(self):
-        return self.db.smembers(self.set_key)
+        return list(self.r.smembers(self.set_key))
 
     def __getitem__(self, key):
         if key not in self:
             raise KeyError()
-        return self.cls(**self.db.hgetall(key))
+        return self.r.oget(key)
 
     def __iter__(self):
         return iter(self.keys())
 
     def __len__(self):
-        return self.db.scard(self.set_key)
+        return self.r.scard(self.set_key)
 
     def __contains__(self, item):
-        return self.db.sismember(self.set_key, item)
+        return self.r.sismember(self.set_key, item)
 
     def __str__(self):
         return str(dict(self))
@@ -175,37 +227,78 @@ class EventTargetTest(TestCase):
     def fired(self, event):
         self.dispatched_event = event
 
-class RedisContainerTest(TestCase):
+class ObjectRedisTest(TestCase):
     class Ship(object):
         def __init__(self, id, type):
             self.id = id
             self.type = type
 
+    @staticmethod
+    def decode(hash):
+        return ObjectRedisTest.Ship(**hash)
+
     def setUp(self):
-        super(RedisContainerTest, self).setUp()
+        self.r = ObjectRedis(StrictRedis(db=15), self.decode)
+        self.r.flushdb()
 
-        self.list = [
-            RedisContainerTest.Ship('0', 'Starfury'),
-            RedisContainerTest.Ship('1', 'Starfury')
-        ]
+        self.objects = {
+            'ship:0': ObjectRedisTest.Ship('ship:0', 'starfury'),
+            'ship:1': ObjectRedisTest.Ship('ship:1', 'frazi')
+        }
+        for key, object in self.objects.items():
+            self.r.hmset(key, vars(object))
 
-        self.db = StrictRedis(db=15)
-        self.db.flushdb()
-        for ship in self.list:
-            self.db.hmset(ship.id, vars(ship))
-            self.db.sadd('ships', ship.id)
+    def test_oget(self):
+        ship = self.r.oget('ship:0')
+        same = self.r.oget('ship:0')
+        self.assertIsInstance(ship, ObjectRedisTest.Ship)
+        self.assertEqual(vars(self.objects['ship:0']), vars(ship))
+        self.assertEqual(id(ship), id(same))
 
-        self.ships = RedisContainer(self.db, 'ships', RedisContainerTest.Ship)
+    def test_oget_destroyed_object(self):
+        ship = self.r.oget('ship:0')
+        destroyed_uid = id(ship)
+        del ship
+        ship = self.r.oget('ship:0')
+        self.assertNotEqual(destroyed_uid, id(ship))
+
+    def test_oget_caching_disabled(self):
+        self.r.caching = False
+        ship = self.r.oget('ship:0')
+        same = self.r.oget('ship:0')
+        self.assertNotEqual(id(ship), id(same))
+
+    def test_omget(self):
+        ships = self.r.omget(self.objects.keys())
+        self.assertEqual(len(self.objects), len(ships))
+        for a, b in zip(self.objects.values(), ships):
+            self.assertEqual(vars(a), vars(b))
+
+class RedisContainerTest(TestCase):
+    def setUp(self):
+        self.r = ObjectRedis(StrictRedis(db=15), ObjectRedisTest.decode)
+        self.r.flushdb()
+
+        self.objects = {
+            'ship:0': ObjectRedisTest.Ship('ship:0', 'starfury'),
+            'ship:1': ObjectRedisTest.Ship('ship:1', 'frazi')
+        }
+        for key, object in self.objects.items():
+            self.r.hmset(key, vars(object))
+            self.r.sadd('ships', key)
+
+        self.ships = RedisContainer(self.r, 'ships')
 
     def test_keys(self):
-        self.assertEqual(self.ships.keys(), set(s.id for s in self.list))
+        self.assertEqual(self.objects.keys(), self.ships.keys())
 
     def test_getitem(self):
-        self.assertEqual(vars(self.ships['0']), vars(self.list[0]))
+        self.assertEqual(vars(self.objects['ship:0']),
+            vars(self.ships['ship:0']))
 
     def test_len(self):
-        self.assertEqual(len(self.ships), len(self.list))
+        self.assertEqual(len(self.objects), len(self.ships))
 
     def test_contains(self):
-        self.assertTrue('0' in self.ships)
+        self.assertTrue('ship:0' in self.ships)
         self.assertFalse('foo' in self.ships)
