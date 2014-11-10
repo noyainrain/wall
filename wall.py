32c613cb4574668609ae6985dfbb93e6cde353d8
Thomas Karmann <thomas@krmnn.de>
2014-11-10 13:39:08 +0100

    added signal handler to intercept ctrl+c and call sys.exit

--- wall.py
+++ wall.py
@@ -7,8 +7,15 @@ from __future__ import (division, absolute_import, print_function,
     unicode_literals)
 
 import sys
+import signal
 import wall
 from wall import WallApp
+
+
+def signal_handler(signal, frame):
+    print('Server shutdown requested by user...')
+    # TODO: maybe do more stuff? e.g. message display, proper closing of connections
+    sys.exit(0)
 
 if __name__ == '__main__':
     # TODO: use option instead
@@ -16,4 +23,8 @@ if __name__ == '__main__':
     print('Wall #{}'.format(wall.release))
     print('display: http://localhost:8080/display')
     print('client:  http://localhost:8080/')
+
+    signal.signal(signal.SIGINT, signal_handler)
+    print('Press Ctrl+C to quit the wall server.')
+
     WallApp(config_path=config_path).run()
