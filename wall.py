d74f095d71f8fa6dc00361bb409964eb862a9b10
Daniel Danner <danner@cs.fau.de>
2014-12-29 00:06:14 +0100

    Make listen address and port configurable
    Also add proper error handling when setting up the listening socket.

--- wall.py
+++ wall.py
@@ -14,6 +14,4 @@ if __name__ == '__main__':
     # TODO: use option instead
     config_path = sys.argv[1] if len(sys.argv) >= 2 else None
     print('Wall #{}'.format(wall.release))
-    print('display: http://localhost:8080/display')
-    print('client:  http://localhost:8080/')
     WallApp(config_path=config_path).run()
