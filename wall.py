25c8216eba4410c792e69a185d154cc567b3f486
Sven <sven.jms@gmail.com>
2015-05-19 18:33:56 +0200

    Lint wall.py

--- wall.py
+++ wall.py
@@ -2,18 +2,25 @@
 
 # Wall
 
+"""Wall server."""
+
 # Python forward compatibility
 from __future__ import (division, absolute_import, print_function,
-    unicode_literals)
+                        unicode_literals)
 
 import sys
 import wall
 from wall import WallApp
 
-if __name__ == '__main__':
+def main(args):
+    """Run the Wall server."""
     # TODO: use option instead
-    config_path = sys.argv[1] if len(sys.argv) >= 2 else None
+    config_path = args[1] if len(args) >= 2 else None
     print('Wall #{}'.format(wall.release))
     print('display: http://localhost:8080/display')
     print('client:  http://localhost:8080/')
     WallApp(config_path=config_path).run()
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
